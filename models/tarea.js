88346bf562ca84b2d96a5984cfba356420a4e5c2
ignaciomoyanosm <ignaciomoyanosm@hotmail.com>
2023-03-21 14:29:04 -0300

    Se sube todo el proyecto realizado

---
+++ models/tarea.js
@@ -0,0 +1,22 @@
+import { v4 } from 'uuid';
+
+class Tarea {
+    id = '';
+    desc = '';
+    completadoEn = null;
+
+    constructor( desc ) {
+
+        this.id = v4();
+        this.desc = desc;
+        this.completadoEn = null;
+        
+    }
+}
+
+
+export {Tarea};
+
+
+
+
